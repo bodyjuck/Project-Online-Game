93c932190fec662746d51b73974779f58c6ce103
Nattapat Tapdungkeow <bodyjuck@gmail.com>
2019-05-07 03:50:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var port = process.env.PORT || 3000;
 var io = require('socket.io')(port);
 
-var player = [];
+var playerNum;
 
 var RandomNum = Math.floor(Math.random() * 100);
 
@@ -12,12 +12,13 @@ console.log(RandomNum);
 io.on("connection",function(socket){
 
     console.log("client connect");
-
     socket.emit('open');
 
-    socket.on("GetNum",function(num){
+    socket.on("Check",function(data){
         
-        socket.emit('get',num);
+        playerNum = data.mynum;
+
+        console.log(playerNum);
         /*
         if(num.GetNum == RandomNum)
         {
