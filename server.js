0deeb1425b32f93ad0cec6e63cc27206d07187a3
Nattapat Tapdungkeow <bodyjuck@gmail.com>
2019-05-07 04:07:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var port = process.env.PORT || 3000;
 var io = require('socket.io')(port);
 
-var playerNum;
+//var playerNum;
 
 var RandomNum = Math.floor(Math.random() * 100);
 
@@ -16,24 +16,29 @@ io.on("connection",function(socket){
 
     socket.on("Check",function(data){
         
-        playerNum = data.mynum;
+        //playerNum = data.mynum;
 
         //console.log(playerNum);
+
+        data = data.mynum;
+        delete data.mynum;
         
-        if(data.mynum == RandomNum)
+        if(data == RandomNum)
         {
             //socket.emit('get',"Win");
-            console.log("Win");
+            //console.log("Win");
         }
-        else if(data.mynum < RandomNum)
+        else if(data < RandomNum)
         {
             //socket.emit('get',"Less");
-            console.log("Less");
+            //console.log("Less");
         }
-        else if(data.mynum > RandomNum)
+        else if(data > RandomNum)
         {
             //socket.emit('get',"Most");
-            console.log("Most");
+            //console.log("Most");
         }
+
+        socket.emit('get',data);
     });
 });
