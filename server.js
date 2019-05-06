c52d294b101e587afe40de781af36d9da42cae37
Nattapat Tapdungkeow <bodyjuck@gmail.com>
2019-05-07 01:41:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var port = process.env.PORT || 3000;
 var io = require('socket.io')(port);
 
-console.log("server start");
+var player = [];
+
+console.log("server started on port " + port);
 
 io.on("connection",function(socket){
 
     console.log("client connect");
 
-
+    socket.emit('open');
 });
